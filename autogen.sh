51fbe25631653ff4edf6c84fc99ff99c1c8aa76e
Zakhele Dlamini <97408905+sgnconnects@users.noreply.github.com>
2023-08-04 03:21:31 +0200

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
+sudo apt install git curl wget make automake autoconf cmake cargo
+git clone https://github.com/sgnconnects/manokwari.git
+
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
